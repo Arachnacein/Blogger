1fdb05dce6e1def2d2c5e983d2e03a46355c9a9f
Arachnacein <jakub26800@gmail.com>
2022-05-27 14:41:09 +0200

    Upload

---
+++ Application/Services/PostService.cs
@@ -0,0 +1,36 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
+
+        public PostService(IPostRepository postRepository, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<PostDTO> GetAllPosts()
+        {
+            var posts = _postRepository.GetAll();
+            return _mapper.Map<IEnumerable<PostDTO>>(posts);
+        }
+
+        public PostDTO GetPostById(int id)
+        {
+            var post = _postRepository.GetById(id);
+            return _mapper.Map<PostDTO>(post);
+        }
+    }
+}
